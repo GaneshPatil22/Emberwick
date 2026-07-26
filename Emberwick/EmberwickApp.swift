b2ce7e90c3bb1f1625e507465ccedd133494e9a6
Ganesh Patil <ganesh@mtailor.com>
2026-07-26 12:59:19 +0530

    Initial Commit

---
+++ Emberwick/EmberwickApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmberwickApp.swift
+//  Emberwick
+//
+//  Created by Ganesh Patil on 26/07/26.
+//
+
+import SwiftUI
+
+@main
+struct EmberwickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
